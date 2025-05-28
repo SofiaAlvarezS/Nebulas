06ffb407bb1a72a0f6cba42b7903684bb74d1c9c
Sofia Alvarez <est.anas.alvarez@unimilitar.edu.co>
2025-05-27 21:33:45 -0500

    el audio

---
+++ Assets/Scripts/Core/MusicPause.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MusicPause : MonoBehaviour
+{
+    [Tooltip("Referencia al MusicPlayer en escena")]
+    public MusicPlayer musicPlayer;
+
+    void Awake()
+    {
+        if (musicPlayer == null)
+            musicPlayer = FindObjectOfType<MusicPlayer>();
+    }
+
+    void OnMouseDown()
+    {
+        if (musicPlayer != null)
+            musicPlayer.Pause();
+        else
+            Debug.LogWarning("MusicPause: no encontré MusicPlayer");
+    }
+}
